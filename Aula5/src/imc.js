977028018d1fd91634b1208e5bafb9ca14a38471
iasr19 <itamar.asr19@gmail.com>
2023-09-08 07:52:15 -0300

    Add Aula 5 - Testes Unitarios

---
+++ Aula5/src/imc.js
@@ -0,0 +1,10 @@
+function calcularIMC(peso, altura) {
+    if (altura === 0) {
+      return 'NaN'; // Retorna "NaN" se a altura for igual a zero
+    }
+  
+    const alturaMetros = altura / 100; // Converte altura de centímetros para metros
+    return (peso / (alturaMetros * alturaMetros)).toFixed(2); // Calcula o IMC com duas casas decimais
+}
+
+module.exports = { calcularIMC }; // Exporta a função calcularIMC
---
+++ Aula5/test/imc.test.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const { calcularIMC } = require('../src/imc');
+
+describe('Calculadora de IMC', () => {
+
+  it('Calculadora deve rodar tranquilo', () => {
+    const peso = 80; // 80 kg
+    const altura = 180; // 180 cm
+
+    const IMEsperado = (peso / Math.pow(altura / 100, 2)).toFixed(2);
+    const IMCCalculado = calcularIMC(peso, altura);
+
+    expect(IMCCalculado).to.equal(IMEsperado);
+  });
+
+
+
+  it('altura = 0, entrada inadequada', () => {
+    const peso = 70; // 70 kg
+    const altura = 0; // 0 cm
+
+    const IMCCalculado = calcularIMC(peso, altura);
+
+    expect(IMCCalculado).to.equal('NaN');
+  });
+});
